7d45de5c54a688dbfb23604bdd68c345f1f5455a
hzahran <hmzahran@gmail.com>
2018-06-14 16:05:31 +0200

    Add basic app structure :muscle:

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore}>
+        <View style={styles.container}>
+
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
